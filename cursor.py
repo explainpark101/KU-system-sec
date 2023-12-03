55249f22d6fe4028c35fbd559c786df070eaacbe
박재형 <47944126+explainpark101@users.noreply.github.com>
2023-12-04 03:35:16 +0900

    ^q^

--- cursor.py
+++ cursor.py
@@ -3,4 +3,7 @@ import ctypes
 hcursor = ctypes.windll.user32.GetCursor()
 
 # ctypes.windll.user32.SetSystemCursor(hcursor, 32514) ## loading
-ctypes.windll.user32.SetSystemCursor(hcursor, 32512) ## default 
+
+if __name__ == "__main__":
+    ctypes.windll.user32.SetSystemCursor(hcursor, 32512) ## default 
+    
